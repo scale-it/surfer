5a3073c88d63430f428fe2525cc8921b2e0429b2
William Hogman <me@whn.se>
2013-03-09 14:38:22 +0100

    Added checking for the return value of Handler.Finish()

--- handler.go
+++ handler.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 )
 
+
 type statet int8
 
 var states = struct {
@@ -108,7 +109,11 @@ func (this *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		this.Render()
 	}
 	if this.state < states.finished {
-		this.Finish()
+		if !this.Finish() {
+			// If finish fails we need to ensure that we don't leave anything hanging
+			this.App.Log.Error("%s, Handler.Finish returned false.", this.Request.URL.Path)
+			http.Error(rw, "Internal Server Error", http.StatusInternalServerError)
+		}
 	}
 }
 
