dbb08e34e779dee2ae893f4a251d35f0b63d3aee
Robert Zaremba <robert.zaremba@zoho.com>
2013-03-24 09:22:57 +0100

    Refine function pattern

--- example/function_pattern.go
+++ example/function_pattern.go
@@ -18,7 +18,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 </body></html>`))
 }
 
-func Counter(app surfer.App, ctx *Context) http.HandlerFunc {
+type Context struct {
+	num int
+	db  map[string]int
+}
+
+func counter(app surfer.App, ctx *Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		app.Log.Debug("Handled index")
 		ctx.num++
@@ -26,15 +31,35 @@ func Counter(app surfer.App, ctx *Context) http.HandlerFunc {
 	}
 }
 
-type Context struct {
-	num int
-	db  map[string]int
+// The idea of above function we can put in more general form
+
+type ContextFunc func(surfer.App, *Context, http.ResponseWriter, *http.Request)
+
+func WithContext(app surfer.App, ctx *Context, f ContextFunc) http.HandlerFunc {
+	// init code
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// prepare code
+		app.Log.Debug("Preparing")
+
+		f(app, ctx, w, r)
+
+		// finish code
+		app.Log.Debug("Finalize")
+	}
+}
+
+func counter2(app surfer.App, ctx *Context, w http.ResponseWriter, r *http.Request) {
+	app.Log.Debug("Handled index")
+	ctx.num++
+	fmt.Fprintf(w, "Hello World, counter: %d", ctx.num)
 }
 
 func main() {
 	app := surfer.New()
 	ctx := new(Context)
-	app.Router.HandleFunc("/", Home)                   // Handle a simple function
-	app.Router.HandleFunc("/index", Counter(app, ctx)) // Handle function which needs a context
+	app.Router.HandleFunc("/", Home)                                  // Handle a simple function
+	app.Router.HandleFunc("/index", counter(app, ctx))                // Handle function which needs a context
+	app.Router.HandleFunc("/index2", WithContext(app, ctx, counter2)) // Handle function which needs a context, with common init / prepare / finish
 	app.Run()
 }
