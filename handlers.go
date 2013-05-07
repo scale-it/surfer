bd72d6c0060ad2b091db819712d7b09567d85295
Robert Zaremba <robert.zaremba@zoho.com>
2013-05-07 03:16:33 +0200

    Added new handlers
    * force https
    * force canonical host
    * sample authentiation wrapper

--- handlers.go
+++ handlers.go
@@ -20,6 +20,7 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type WithHTTPLogger struct {
@@ -28,21 +29,64 @@ type WithHTTPLogger struct {
 }
 
 func (this WithHTTPLogger) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	username := "-"
+	username := ""
 	if req.URL.User != nil {
 		if name := req.URL.User.Username(); name != "" {
 			username = name
 		}
 	}
+	start := time.Now()
+	this.Next.ServeHTTP(w, req)
+	elapsed := float64(time.Since(start)) / float64(time.Millisecond)
 
-	fmt.Fprintf(this.Writer, "%s - %s \"%s %s %s\"\n",
+	fmt.Fprintf(this.Writer, "%s - %s \"%s %s %s\". Elapsed: %f ms\n",
 		strings.Split(req.RemoteAddr, ":")[0],
 		username,
 		req.Method,
 		req.RequestURI,
 		req.Proto,
 		// status,
-	)
+		elapsed)
+}
+
+// Handler which check if request is from canonical host and uses https. Otherwise will
+// redirect to https://<canonicalhost>/rest/of/the/url
+type WithForceHTTPS struct {
+	CanonicalHost string
+	Next          http.Handler
+}
+
+func (this WithForceHTTPS) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	is_http := true
+	if h, ok := req.Header["X-Forwarded-Proto"]; ok {
+		if h[0] == "https" {
+			is_http = false
+		}
+	}
+	hostPort := strings.Split(req.Host, ":")
+	if is_http || hostPort[0] != this.CanonicalHost {
+		hostPort[0] = this.CanonicalHost
+		url := "https://" + strings.Join(hostPort, ":") + req.URL.String()
+		http.Redirect(w, req, url, http.StatusMovedPermanently)
+		return
+	}
 
 	this.Next.ServeHTTP(w, req)
+}
+
+type Authenticator func(req *http.Request) bool
+
+// Enusres authentication for handlers. Otherwise call fallback.
+type WithAuth struct {
+	A        Authenticator
+	Next     http.Handler
+	Fallback http.Handler
+}
+
+func (this WithAuth) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if this.A(req) {
+		this.Next.ServeHTTP(w, req)
+	} else {
+		this.Fallback.ServeHTTP(w, req)
+	}
 }
